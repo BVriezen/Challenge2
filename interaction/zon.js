5a7ba8af7b32d57fae9e37cd9a77804a206a8a4f
Bas Vriezen <basvriezen@bass-mbp.wireless.hhs.nl>
2018-03-19 16:10:56 +0100

    Moon Id changed

--- interaction/zon.js
+++ interaction/zon.js
@@ -4,11 +4,17 @@ var tl1 = new TimelineMax();
 // tl1.to(zon, 1, {top: 40});
 // tl1.to(zon, 1, {top: 200, ease:Power0.easeOut});
 // tl1.to(zon, 1, {top: 200, ease:Bounce.easeOut});
-tl1.to(zon, 1, {top: 40, ease:Elastic.easeOut});
+tl1.to(sun, 1, {top: 40, ease:Elastic.easeOut});
 // tl1.to(zon, 1, {top: 200, ease:Elastic.easeIn})
 
-// var moon = document.getElementById('half-circle')();
+var information = document.getElementById('info');
 
-// var tl2 = new TimelineMax();
-// tl2.to(half-circle, 1{top: 200, ease:Elastic.easeOut});
+var tl3 = new TimelineMax();
+
+tl3.to(information, 1, {opacity: 1.0, ease:Power2.easeIn});
+
+var moon = document.getElementById('half');
+
+var test = new TimelineMax();
+test.to(moon, 1, {top: 40, ease:Elastic.easeOut});
 
